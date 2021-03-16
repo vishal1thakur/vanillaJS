74c712cc0dacd702af09facc8840a04cca6fcac5
Vishal Thakur <interaction.vishal@gmail.com>
2021-03-17 04:49:39 +0530

    JS for DELETE request

--- HTTP Library/library.js
+++ HTTP Library/library.js
@@ -49,3 +49,17 @@ easyHTTP.prototype.put = function (url, data, callback) {
 };
 
 //4) Make an HTTP DELETE Request
+easyHTTP.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true);
+
+  let self = this;
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, 'Post Deleted');
+    } else {
+      callback('Error: ' + self.http.status);
+    }
+  };
+
+  this.http.send();
+};
--- HTTP Library/script.js
+++ HTTP Library/script.js
@@ -3,22 +3,22 @@ const http = new easyHTTP();
 
 // ----------- B) Call requests
 // 1a) Get Posts
-// http.get('http://jsonplaceholder.typicode.com/posts', function (err, posts) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(posts);
-//   }
-// });
+http.get('http://jsonplaceholder.typicode.com/posts', function (err, posts) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+});
 
 // 1b) Get Single Post
-// http.get('http://jsonplaceholder.typicode.com/posts/1', function (err, post) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
+http.get('http://jsonplaceholder.typicode.com/posts/1', function (err, post) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
 
 // 2) Send Post
 // i) Create data
@@ -28,17 +28,17 @@ const data = {
 };
 
 // ii) Create Post
-// http.post(
-//   'http://jsonplaceholder.typicode.com/posts',
-//   data,
-//   function (err, post) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log(post);
-//     }
-//   }
-// );
+http.post(
+  'http://jsonplaceholder.typicode.com/posts',
+  data,
+  function (err, post) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(post);
+    }
+  }
+);
 
 // 3) PUT (Update)
 http.put(
@@ -49,6 +49,18 @@ http.put(
       console.log(err);
     } else {
       console.log(post);
+    }
+  }
+);
+
+// 1d) Delete
+http.delete(
+  'http://jsonplaceholder.typicode.com/posts/1',
+  function (err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(response);
     }
   }
 );
