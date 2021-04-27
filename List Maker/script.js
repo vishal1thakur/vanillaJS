abc558bf16c7e8818580e2935d3869b285b2d71c
Vishal Thakur <interaction.vishal@gmail.com>
2021-04-27 18:40:05 +0530

    Checkmark and delete functionality

--- List Maker/script.js
+++ List Maker/script.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 // Functions
 function addTodo(e) {
@@ -31,4 +32,19 @@ function addTodo(e) {
   todoList.appendChild(todoDiv);
   // Clear Todo input value
   todoInput.value = '';
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+  // Delete Todo
+  if (item.classList[0] === 'trash-btn') {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+
+  // Check mark
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
 }
