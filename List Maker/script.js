9c611829806283b74377a308b0361350ebe96a87
Vishal Thakur <interaction.vishal@gmail.com>
2021-04-27 18:17:55 +0530

    Styling for todo entry

--- List Maker/script.js
+++ List Maker/script.js
@@ -14,7 +14,7 @@ function addTodo(e) {
   todoDiv.classList.add('todo');
   // Create LI
   const newTodo = document.createElement('li');
-  newTodo.innerText = 'hey';
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add('todo-item');
   todoDiv.appendChild(newTodo);
   // Checkmark button
@@ -25,8 +25,10 @@ function addTodo(e) {
   // Trash button
   const trashButton = document.createElement('button');
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add('complete-btn');
+  trashButton.classList.add('trash-btn');
   todoDiv.appendChild(trashButton);
   // Attach div to ul
   todoList.appendChild(todoDiv);
+  // Clear Todo input value
+  todoInput.value = '';
 }
